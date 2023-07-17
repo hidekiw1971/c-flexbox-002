c942c768388e90e1c83221cd704f686b618077b8
hideki watabe <hideki.w.1971@gmail.com>
2023-07-18 07:49:08 +0900

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+jQuery(function ($) {
+  // この中であればWordpressでも「$」が使用可能になる
+  // setting
+  var swiper = new Swiper('.swiper', {
+    autoplay: {
+      delay: 1000
+    },
+    loop: true
+  });
+  // setting
+});
